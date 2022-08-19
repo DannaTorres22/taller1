1b00df90d98543f719c39225d978a3e1b6ebe619
DannaTorres22 <dltorres862@misena.edu.co>
2022-08-19 17:28:09 -0500

    taller_1

---
+++ Practicasjs/js/index.js
@@ -0,0 +1,10 @@
+
+//definimos las variables 
+let gradosF,gradosC
+
+gradosF=prompt("Ingrese la cantidad de grados fahrenheit a convertir")
+
+gradosF=parseInt(gradosF)
+gradosC=(gradosF-32)/1.8
+
+console.log("los grados fahrenheit equivalen a "+gradosC+" grados centigrados")
