4d52e46de19ca5c986f5a0816441b97723792a75
DannaTorres22 <104436270+DannaTorres22@users.noreply.github.com>
2022-09-18 22:35:45 -0500

    avance5

--- js/eje6.js
+++ js/eje6.js
@@ -1,7 +1,7 @@
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
-const expresiones = {
+const expresiones = { 
 	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
